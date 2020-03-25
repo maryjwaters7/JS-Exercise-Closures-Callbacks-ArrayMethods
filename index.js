4ba9f5985bbb8e4c5b270665471d99cfc04830c3
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-03-24 21:43:40 -0400

    updated all regular challenge files except counterMaker

--- index.js
+++ index.js
@@ -247,8 +247,11 @@ function stringSmash(strings) {
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  const names = runners.map(function(item){
+    return `${item.last_name}, ${item.first_name}`
+  })
+  return names;
 }
 
 /**
@@ -263,8 +266,11 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  const caps = runners.map(function (item){
+    return item.first_name.toUpperCase();
+  })
+  return caps;
 }
 
 /**
