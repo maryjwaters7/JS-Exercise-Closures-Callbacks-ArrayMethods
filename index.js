dfeb1e41f0d91f0c1f5af9f35e3aa29fce97308c
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-03-25 01:29:36 -0400

    updated another strect file

--- index.js
+++ index.js
@@ -311,8 +311,11 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  let num = runners.reduce(function(accumulator, item){
+    return (accumulator + item.donations)
+  }, 0);
+return num;
 }
 
 /////////////// CLOSURES ///////////////
